6a08cb62945e83640c8d13548207d8323b4c66ba
Randy Ortiz <Randyortiz28@gmail.com>
2021-04-08 12:57:41 -0400

    installed packages and setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/', (_, res) => {
+  res.json({ data: 'API is accounted for' })
+})
+
+app.listen(port, () => {
+  console.log(`Server is alive on port ${port}`)
+})
+
+// console.log('it works!')
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USER) //only for mac users
+// console.log(process.env.GREET)
